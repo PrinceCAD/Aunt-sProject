6605ec6d579bc165d5fc8a157322841ff922e36c
PrinceCAD Studios <114841148+PrinceCAD@users.noreply.github.com>
2024-05-19 19:02:52 +0100

    new

--- code.js
+++ code.js
@@ -1,6 +1,16 @@
 import { NavigationEvent,navGainColor } from "./subcodes/navbar.js"
 import { HomeFunctions } from "./subcodes/Home.js";
-
+export var backgrounds = [
+  "../imgs/home.jpg",
+  "../imgs/new/artem-kovalev-fk3XUcfTAvk-unsplash.jpg",
+  "../imgs/new/ben-white-e92L8PwcHD4-unsplash.jpg",
+  "../imgs/new/home.jpg",
+  "../imgs/new/marcos-paulo-prado-LlrQF5JFRIs-unsplash.jpg",
+  "../imgs/new/nik-shuliahin-BuNWp1bL0nc-unsplash.jpg",
+  "../imgs/new/taylor-hernandez-NK-N6coeI5Y-unsplash.jpg",
+  "../imgs/new/valentina-ivanova-IkaQsoxmkmU-unsplash.jpg",
+  "../imgs/new/yingchou-han-IJrIeCs3D4g-unsplash.jpg",
+];
 HomeFunctions()
 
 window.addEventListener('scroll', () =>{
--- getDOM.js
+++ getDOM.js
@@ -1,7 +1,12 @@
 const header = document.getElementById('bar')
 const screen = document.getElementById('screen')
+const HomePage = document.getElementById('Home');
+const logo = document.getElementById('shift_logo');
+const B_Left = document.getElementById("background_left");
+const B_Right = document.getElementById("background_right");
 const clicks = document.getElementById('HomeCarrosel')
 const messageDiv = document.getElementById('HomeStories')
 const nav = document.getElementsByClassName('nav-link')
+const navLinks = document.getElementsByClassName('nav-link')
 
-export {header, screen, nav, messageDiv, clicks}
+export {header, screen, nav, messageDiv, clicks, navLinks, logo, B_Left, B_Right, HomePage}
